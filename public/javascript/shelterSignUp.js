c4b6baca1800f73afae83f651ac658e3649734b9
Pratham Rawat <prathamscience@gmail.com>
2018-08-02 14:14:34 -0400

    Create a signup page for shelters to use, add and update links in signin and create account pages

--- public/javascript/shelterSignUp.js
+++ public/javascript/shelterSignUp.js
@@ -2,17 +2,32 @@
   const txtName=document.getElementById('txtName');
   const txtEmail=document.getElementById('txtEmail');
   const txtPassword=document.getElementById('txtPassword');
+  const phoneNumberElement = document.getElementById('phone');
+  const managerNameElement= document.getElementById('acctManager');
+  const streetAddressElement = document.getElementById("streetAddress");
+  const cityElement = document.getElementById("city");
+  const zipCodeElement = document.getElementById("zipCode");
   const btnSignUp=document.getElementById('btnSignUp');
-  const shelterSignUpElement = document.getElementById("shelterSignUp");
   const auth=firebase.auth();
   var name;
-  shelterSignUpElement.addEventListener("click", e => {
-    window.location.replace("shelterSignUp.html");
-  });
   btnSignUp.addEventListener('click',e => {
-    name=txtName.value;
-    const email=txtEmail.value;
-    const pass=txtPassword.value;
+    const name = txtName.value;
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const phone = phoneNumberElement.value;
+    const manager = managerNameElement.value;
+    const street = streetAddressElement.value;
+    const city = cityElement.value;
+    const zip = zipCodeElement.value;
+    var db = firebase.firestore();
+    db.collection("Shelters").doc().set({
+      sheltername: name,
+      address: street,
+      city: city,
+      manager: manager,
+      zip: zip,
+      phone: phone
+    })
     const promise=auth.createUserWithEmailAndPassword(email,pass).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
@@ -21,13 +36,16 @@
         alert('The password is too weak.');
       } else if(errorCode=='auth/invalid-email'){
         alert('The email is invalid')
+      } else if (zip.length != 4){
+        alert("Please input a valid Zip Code");
+      } else if (phone != 10) {
+        alert("Please input a Valid Phone Number (do not include dashes!)");
       } else {
-        alert(errorMessage);
+        alert(errorMessage)
       }
       console.log(error);
       promise.catch(e=>console.log(e.message));
-    });
-  });
+    })});
   firebase.auth().onAuthStateChanged(firebaseUser=>{
     if (firebaseUser){
       const user = auth.currentUser;
