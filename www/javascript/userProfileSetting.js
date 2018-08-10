7ac9f298644a4b21049d475afccecefa177bdfd5
Pratham Rawat <prathamscience@gmail.com>
2018-08-10 13:45:28 -0400

    Retrive account data from database in accounts settings page and create basic framework for displaying data.

--- www/javascript/userProfileSetting.js
+++ www/javascript/userProfileSetting.js
@@ -1,15 +1,25 @@
 (function(){
   var auth = firebase.auth();
   var name;
+  const userNameElement = document.getElementById("userName");
+  const userAgeElement = document.getElementById("age");
+  const userPasswordElement = document.getElementById("password");
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       name=auth.currentUser.displayName;
       var img2 = document.getElementById('myimg2');
-      if(user.photoURL==null){
+      userNameElement.value = name;
+      if(user.photoURL == null) {
         img2.src="../images/white.png";;
       }else{
         img2.src = user.photoURL;
       }
+      if(user.age == null) {
+        userAgeElement.placeholder = "No Age Detected"
+      } else {
+        userAgeElement.value = user.age;
+      }
+      userPasswordElement.value = auth.password;
     }else {
         console.log("boi");
       }
