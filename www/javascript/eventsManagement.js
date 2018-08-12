6dce21a23ad50953e729127abdc905b490cfac45
ShelterLinks <41589854+ShelterLinks@users.noreply.github.com>
2018-08-12 00:49:30 -0400

    Add files via upload

--- www/javascript/eventsManagement.js
+++ www/javascript/eventsManagement.js
@@ -1,36 +1,142 @@
 var db = firebase.firestore();
 var auth = firebase.auth();
 var name;
+var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var idOfUser;
+var email;
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     name=auth.currentUser.displayName;
     var img2 = document.getElementById('myimg2');
+    email=auth.currentUser.email;
     if(user.photoURL==null){
       img2.src="../images/white.png";
     }else{
       img2.src = user.photoURL;
     }
-  }else {
-      console.log("boi");
-    }
-  });
-(function(){
-    db.collection("Events").where("organization", "==", name)
+    db.collection("Shelters").where("email", "==", email)
     .get()
     .then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshot
-        var date=doc.data().date;
-        var counter=0;
-        $("#events").append("<div class=\"myEvent\"><div class=\"insertedEvent\"><a class=\"eventsLink\">"+
-        "<h2 class=\"eventOrg\">"+doc.data().organization+"</h2>"+
-        "<br><h6 class=\"eventName\">Type: "+doc.data().name+" </h6>"+
-        "<h6 class=\"eventTime\"> Date: "+date+"</h6>"+
-        "<br><h6 class=\"eventVolunteers\">Volunteer Spots Remaining: "+doc.data().numOfVolunteers+"</h6></a><div>")
-        counter++;
+        idOfUser=doc.id;
+      })
+    });
+  }else {
+    console.log("boi");
+  }
+});
+(function(){
+  db.collection("Events").where("organization", "==", name).orderBy("startDate")
+  .get()
+  .then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      var ourData=doc.data();
+      var name = doc.data().name;
+      var startDate = doc.data().startDate;
+      var day = new Date(startDate);
+      var dayOfWeek =day.getDay();
+      dayOfWeek=days[dayOfWeek];
+      var months=["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+      var month = day.getMonth();
+      month=months[month];
+      var day = startDate.substring(3,5);
+      if (day.indexOf("0")==0){
+        day=day.substring(1);
+      }
+      var endDate = doc.data().endDate;
+      var startTime = doc.data().startTime;
+      var endTime = doc.data().endTime;
+      var borough=doc.data().borough;
+      var numOfVolunteerRemaining=doc.data().numOfVolunteerRemaining;
+      if (borough=="Bronx"){
+        borough="the Bronx";
+      }
+      var dateString = "";
+
+      if(startDate == endDate) {
+        dateString = dayOfWeek + ", " + startTime + " - " + endTime;
+      } else if (endDate == "Ongoing" || endTime == "Ongoing") {
+        dateString = dayOfWeek + ", " + startTime + " - Ongoing" ;
+      } else if (endDate != startDate) {
+        dateString = dayOfWeek + " - " + endDate + " | " + startTime + " - " + endTime;
+      }
+      var location = doc.data().location;
+      var locationURL = encodeURI(location);
+      var imageSrc = "";
+      if(doc.data().imagePath == null) {
+        imageSrc = "https://maps.googleapis.com/maps/api/staticmap?"+ "center=" + locationURL + "&size=1024x250&markers=red" + "&key=AIzaSyCQgxFBpg7lC0ij4Z_q-kSG9M11W58wof0";
+      } else {
+        imageSrc = doc.data().imagePath;
+      }
+      var volunteers = doc.data().numOfVolunteer;
+      var organization = doc.data().organization;
+      var counter=0;
+      var tagString = "";
+      doc.data().tags.forEach(function(tag) {
+        tagString += "<span class=\"eventTag\"> " + tag +" </span>";
+      });
+      $("#events").append("<div class=\"myEvent\" ><div class=\"insertedEvent\"><a class=\"eventsLink\" id=\""+organization+"\">"+
+      "<div class=\"eventInformation\"id=\""+organization+"\">" +
+      "<div class=\"eventImageDiv\"><img class=\"eventImage\" src=\"" + imageSrc + "\" alt=\"Map Location\"></div>" +
+      "<div class=\"inlineboi\"><img class=\"shelterImage\" src=\"\"/>"+
+      "<h1 class=\"eventName\">" + name + "</h1>" +
+      "<h6 class=\"eventTime\">"+ dateString +" in "+borough+"<span class=\"nope\">"+startDate+"</span></h6>" +
+      "<h3 class=\"easyDate\">"+month+"<br>"+day+"</h3>"+
+      "<div class=\"volunteer\"><button id=\""+doc.id+"\" type=\"button\" class=\"btn btn-primary volunteered update\">Update Event</button>"+
+      "<button type=\"button\" class=\"btn btn-primary volunteered confirm\">Confirm Volunteers</button></div></div></div>")
+      counter++;
+    });
+    var update=document.getElementsByClassName("update");
+    for (var i=0;i<update.length;i++){
+      update[i].addEventListener('click',e => {
+        console.log(e.path[0].id);
+        db.collection("Events").doc(e.path[0].id)
+        .get()
+        .then(function(doc) {
+          return db.collection("Events").doc(doc.id).update({
+            updateEvent: true
+          })
+          .then(function() {
+            window.location.href="updateEvent.html";
+          })
+        })
+        .catch(function(error) {
+          console.log("Error getting documents: ", error);
+        });
+      })
+    }
+  })
+  .catch(function(error) {
+    console.log("Error getting documents: ", error);
+  });
+  document.onclick = function(event) {
+    var target = $(event.target.parentElement.parentElement);
+    var target2 = $(event.target.parentElement);
+    var data=target[0].innerHTML;
+    var eventNamed=data.substring(data.indexOf("eventName")+11,data.indexOf("<",data.indexOf("eventName")+11));
+    var startDatee=data.substring(data.indexOf("nope")+6,data.indexOf("<",data.indexOf("nope")+7));
+    var timed=data.indexOf("eventTime");
+    var timeds=data.indexOf(",",timed+1);
+    var startTimed=data.substring(timeds+2,(data.indexOf("-",timed+1))-1).replace(" ","");
+    var organization=target[0].id;
+    console.log(startTimed);
+    console.log(organization);
+    console.log(eventNamed);
+    console.log(startDatee);
+    db.collection("Events").where("organization", "==", organization).where("startDate", "==",startDatee).where("startTime", "==", startTimed).where("name", "==", eventNamed)
+    .get()
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+        return db.collection("Shelters").doc(idOfUser).update({
+          shelterEventPreview: doc.id
+        })
+        .then(function() {
+          window.location.replace("shelterEventPreview.html");
+        })
       });
     })
     .catch(function(error) {
       console.log("Error getting documents: ", error);
     });
+  };
 }());
--- www/javascript/individualCoupon.js
+++ www/javascript/individualCoupon.js
@@ -1,96 +1,90 @@
 var currentSheltercoins;
 var couponRedirect;
-var company;
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    name=auth.currentUser.displayName;
-    var img2 = document.getElementById('myimg2');
-    if(user.photoURL==null){
-      img2.src="../images/white.png";
-    }else{
-      img2.src = user.photoURL;
-    }
-    console.log(user.photoURL);
-    console.log(name);
-  }else {
-    console.log("boi");
-  }
-});
+var correctId;
+var email;
+var db = firebase.firestore();
 (function(){
-  var db = firebase.firestore();
-  db.collection("Coupons").where("showCoupon", "==", true)
-  .get()
-  .then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      var ourData=doc.data();
-      company=ourData.business;
-      $("#couponInfo").append("<img class=\"couponImage\" src=\""+ourData.image+"\"><h1 class=\"couponName\">"+ourData.business+"</h1><h2 class=\"couponDescription\">"+ourData.description+
-      "<br><a id=\"couponRedirect\" class=\"btn btn-primary redeem\" href=\"#\" role=\"button\">Open QR Scanner</a>"+
-      "<a id=\"maps\" class=\"btn btn-primary map\" href=\"https://www.google.com/maps/search/?api=1&query="+company+"\" role=\"button\">Open in Maps</a>"+
-      "<h5 id=\"redeemHead\">REDEEMING YOUR COUPON WITH SHELTERCOINS IS SIMPLE:</h5>"+
-      "<ol id=\"directions\">"+
-      "<li>After choosing the coupon that you want to redeem, go to the business associated with the coupon.</li>"+
-      "<li>Open the QR Code Scanner above directly in the app.</li>"+
-      "<li>Go to the register and scan the QR code with the ShelterLinks Logo</li>"+
-      "<li>After scanning, the required number of points will be deducted.</li>"+
-      "<li>Present the coupon verification page to the cashier.</li>"+
-      "<li>Enjoy your food!</li>"+
-      "</ol>"+
-      "<div class=\"panel-group\" id=\"faqAccordion\"><div class=\"panel panel-default \"><div class=\"panel-heading accordion-toggle question-toggle collapsed\" data-toggle=\"collapse\" data-parent=\"#faqAccordion\" data-target=\"#question0\">"+
-      "<h4 class=\"panel-title\"><a href=\"#\" id=\"boied\" class=\"ing\">Click for Terms and Services</a></h4></div>"+
-      "<div id=\"question0\" class=\"panel-collapse collapse\" style=\"height: 0px;\"><div class=\"panel-body\"><p id=\"termsServed\">"+ourData.termsAndServices+"</p></div></div></div></div>");
-      return db.collection("Coupons").doc(doc.id).update({
-        showCoupon: false
-      })
-    });
-    couponRedirect=document.getElementById("couponRedirect");
-    couponRedirect.addEventListener('click',e => {
-      db.collection("Users").where("name", "==", name)
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      name=auth.currentUser.displayName;
+      var img2 = document.getElementById('myimg2');
+      email=auth.currentUser.email;
+      db.collection("Users").where("email", "==", email)
       .get()
       .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-          currentSheltercoins=doc.data().points;
-          console.log(currentSheltercoins);
-          if(currentSheltercoins<300){
-            alert("Sorry, you do not have enough points to redeem this coupon.");
-          }else{
-            cordova.plugins.barcodeScanner.scan(
-              function (result) {
-                var correctCode="ShelterLinks Redemption: "+company;
-                if (result.text==correctCode){
-                  currentSheltercoins-=300;
-                  return db.collection("Coupons").doc(doc.id).update({
-                    points:currentSheltercoins,
-                    isRedeemed:true
-                  })
-                  .then(function() {
-                    window.location.replace("couponRedeemed.html");
-                    alert("Yes! It Works!")
-                  })
-                }else{
-                  alert("This is not the correct QR code, or you are using the incorrect QR scanner")
-                }
-              },
-              function (error) {
-                alert("Scanning failed: " + error);
-              },
-              {
-                preferFrontCamera : false, // iOS and Android
-                showFlipCameraButton : true, // iOS and Android
-                showTorchButton : true, // iOS and Android
-                torchOn: true, // Android, launch with the torch switched on (if available)
-                saveHistory: true, // Android, save scan history (default false)
-                prompt : "Place a barcode inside the scan area", // Android
-                resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-                formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-                orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-                disableAnimations : true, // iOS
-                disableSuccessBeep: false // iOS and Android
+        querySnapshot.forEach(function(doc1) {
+          correctId=doc1.data().individualCoupon;
+          db.collection("Coupons").doc(correctId)
+          .get()
+          .then(function(doc) {
+            var ourData=doc.data();
+            var company=ourData.business;
+            $("#couponInfo").append("<img class=\"couponImage\" src=\""+ourData.image+"\"><h1 class=\"couponName\">"+ourData.business+"</h1><h2 class=\"couponDescription\">"+ourData.description+
+            "<br><a id=\"couponRedirect\" class=\"btn btn-primary redeem\" href=\"#\" role=\"button\">Open QR Scanner</a>"+
+            "<a id=\"maps\" class=\"btn btn-primary map\" href=\"https://www.google.com/maps/search/?api=1&query="+company+"\" role=\"button\">Open in Maps</a>"+
+            "<h5 id=\"redeemHead\">REDEEMING YOUR COUPON WITH SHELTERCOINS IS SIMPLE:</h5>"+
+            "<ol id=\"directions\">"+
+            "<li>After choosing the coupon that you want to redeem, go to the business associated with the coupon.</li>"+
+            "<li>Open the QR Code Scanner above directly in the app.</li>"+
+            "<li>Go to the register and scan the QR code with the ShelterLinks Logo</li>"+
+            "<li>After scanning, the required number of points will be deducted.</li>"+
+            "<li>Present the coupon verification page to the cashier.</li>"+
+            "<li>Enjoy your food!</li>"+
+            "</ol>"+
+            "<div class=\"panel-group\" id=\"faqAccordion\"><div class=\"panel panel-default \"><div class=\"panel-heading accordion-toggle question-toggle collapsed\" data-toggle=\"collapse\" data-parent=\"#faqAccordion\" data-target=\"#question0\">"+
+            "<h4 class=\"panel-title\"><a href=\"#\" id=\"boied\" class=\"ing\">Click for Terms and Services</a></h4></div>"+
+            "<div id=\"question0\" class=\"panel-collapse collapse\" style=\"height: 0px;\"><div class=\"panel-body\"><p id=\"termsServed\">"+ourData.termsAndServices+"</p></div></div></div></div>");
+            couponRedirect=document.getElementById("couponRedirect");
+            couponRedirect.addEventListener('click',e => {
+              currentSheltercoins=doc1.data().points;
+              if(currentSheltercoins<300){
+                alert("Sorry, you do not have enough points to redeem this coupon.");
+              }else{
+                cordova.plugins.barcodeScanner.scan(
+                  function (result) {
+                    var correctCode="ShelterLinks Redemption: "+company;
+                    if (result.text==correctCode){
+                      currentSheltercoins-=300;
+                      return db.collection("Users").doc(doc1.id).update({
+                        points:currentSheltercoins,
+                        couponRedeemed:doc.id
+                      })
+                      .then(function() {
+                        window.location.replace("couponRedeemed.html");
+                      })
+                    }else{
+                      alert("This is not the correct QR code, or you are using the incorrect QR scanner")
+                    }
+                  },
+                  function (error) {
+                    alert("Scanning failed: " + error);
+                  },
+                  {
+                    preferFrontCamera : false, // iOS and Android
+                    showFlipCameraButton : true, // iOS and Android
+                    showTorchButton : true, // iOS and Android
+                    torchOn: true, // Android, launch with the torch switched on (if available)
+                    saveHistory: true, // Android, save scan history (default false)
+                    prompt : "Place a barcode inside the scan area", // Android
+                    resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                    formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+                    orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+                    disableAnimations : true, // iOS
+                    disableSuccessBeep: false // iOS and Android
+                  }
+                );
               }
-            );
-          }
-        });
-      })
-    });
-  })
+            });
+          });
+        })
+      });
+      if(user.photoURL==null){
+        img2.src="../images/white.png";
+      }else{
+        img2.src = user.photoURL;
+      }
+    }else {
+      console.log("boi");
+    }
+  });
 }());
